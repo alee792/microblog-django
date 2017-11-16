30749b4d2d30112b0635409da28fac54f97f4b77
Anthony Lee <alee792@gmail.com>
2017-11-16 17:58:33 -0500

    Broke registration, so that's cool

--- posts/backends.py
+++ posts/backends.py
@@ -1,9 +1,10 @@
 from registration.backends.simple.views import RegistrationView
+from django.shortcuts import render, redirect
 
 # my new registration view, subclassing RegistrationView
 # from our plugin
 class MyRegistrationView(RegistrationView):
-    def get_success_url(self, request, user):
+    def get_success_url(self, request):
         # the named URL that we want to redirect to after
         # successful registration
-        return ('registration_create_post')
+        return render(request, 'registration_complete.html')
